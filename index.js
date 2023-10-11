b2274d92cf141cf1150392b5ea8a0cfa3f4b9520
RyckxDev <ryan.dag42@gmail.com>
2023-10-11 16:17:28 -0300

    fiz tudo q tem direito

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Olá a todos =D")
+/*
+- Variáveis ☑
+- Operadores ☑
+- Laços de repetição ☑
+- Estruturas de decisões ☑
+*/
+
+let nomeDoBoneco = "Felipao"
+let xpDoBoneco = 11.000
+let rankingDoBoneco = ""
+let dado = 0;
+
+if(xpDoBoneco <= 1.000){
+    rankingDoBoneco = "Ferro"
+} else if(xpDoBoneco >= 1.001 && xpDoBoneco <= 2.000){
+    rankingDoBoneco = "Bronze"
+} else if(xpDoBoneco >= 2.001 && xpDoBoneco <= 5.000){
+    rankingDoBoneco = "Prata"
+} else if(xpDoBoneco >= 5.001 && xpDoBoneco <= 7.000){
+    rankingDoBoneco = "Ouro"
+} else if(xpDoBoneco >= 7.001 && xpDoBoneco <= 8.000){
+    rankingDoBoneco = "Platina"
+} else if(xpDoBoneco >= 8.001 && xpDoBoneco <= 9.000){
+    rankingDoBoneco = "Ascendente"
+} else if(xpDoBoneco >= 9.001 && xpDoBoneco <= 10.000){
+    rankingDoBoneco = "Imortal"
+} else {
+    rankingDoBoneco = "Radiante"
+}     
+
+while (dado <= 1) {
+    dado = Math.floor(Math.random() * 10);
+}
+     
+console.log("O Herói de nome " + nomeDoBoneco + " está no nível " + rankingDoBoneco + " e ganhou um total de", dado, "poçoes de vida!")
